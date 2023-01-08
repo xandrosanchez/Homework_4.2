cd7b61051fd5db4cd0240b1292ed84c221379b60
Alex <din.myrk@gmail.com>
2023-01-08 21:06:55 +0300

    4.31 Внесены исправления

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -1,7 +1,7 @@
-- liquibase formatted sql
+-- liquibase formatted sql
 
-- changeset aSnegireff:1
+-- changeset aSnegireff:1
     CREATE INDEX student_name_index ON student (name);
 
-- changeset aSnegireff:2
+-- changeset aSnegireff:2
 CREATE INDEX faculty_name_color_index ON faculty (name, color);
